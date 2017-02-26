b07cc5a5b40885286b6df9af1af976b103624591
Eatowl <csnelcov@gmail.com>
2017-02-27 01:45:59 +0700

    add list device

--- main.c
+++ main.c
@@ -3,11 +3,20 @@
 #include <stdio.h>
 
 int main() {
-	char *dev, errbuf[PCAP_ERRBUF_SIZE];
-	if ((dev = pcap_lookupdev(errbuf)) == NULL) {
-		perror("pcap_lookupdev error");
+	pcap_if_t *alldevsp, *device;
+	char errbuf[PCAP_ERRBUF_SIZE];
+	if (pcap_findalldevs(&alldevsp, errbuf) == -1) {
+		perror("pcap_findalldevs error");
 		exit(1);
 	}
-	printf("DEV: %s\n", dev);
+	for (device = alldevsp; device != NULL; device = device->next) {
+		printf("DEVICE: %s - %s \n", device->name, device->description);
+	}
+	char work_dev[] = "eth0";
+	pcap_t *handle;
+	if ((handle = pcap_open_live(work_dev, BUFSIZ, 1, 1000, errbuf)) == NULL) {
+		perror("pcap_open_live error");
+		exit(1);
+	}
 	return 0;
 }
