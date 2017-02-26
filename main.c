527407261c5fbad3892ffa93cd14f22bf1962ce0
Eatowl <csnelcov@gmail.com>
2017-02-26 23:27:43 +0700

    ...

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <pcap.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int main() {
 	char *dev, errbuf[PCAP_ERRBUF_SIZE];
-	dev = pcap_lookupdev(errbuf);
+	if ((dev = pcap_lookupdev(errbuf)) == NULL) {
+		perror("pcap_lookupdev error");
+		exit(1);
+	}
 	printf("DEV: %s\n", dev);
 	return 0;
 }
