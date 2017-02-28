4be32d95abfcecd18de77209ea20d911ca8b2d0d
Eatowl <csnelcov@gmail.com>
2017-02-28 15:29:39 +0700

    add choice device

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <pcap.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_BUF 32
+
+void listDevice(pcap_if_t *alldevsp);
 
 int main() {
 	pcap_if_t *alldevsp, *device;
@@ -9,14 +14,37 @@ int main() {
 		perror("pcap_findalldevs error");
 		exit(1);
 	}
-	for (device = alldevsp; device != NULL; device = device->next) {
-		printf("DEVICE: %s - %s \n", device->name, device->description);
-	}
-	char work_dev[] = "eth0";
+	listDevice(alldevsp);
 	pcap_t *handle;
-	if ((handle = pcap_open_live(work_dev, BUFSIZ, 1, 1000, errbuf)) == NULL) {
-		perror("pcap_open_live error");
+	int number, count = 0;
+	printf("\nInput number device: ");
+	if ((scanf("%d", &number)) == -1) {
+		perror("scanf error");
 		exit(1);
 	}
+	device = alldevsp;
+	while (device != NULL) {
+		if (number == count) {
+			if ((handle = pcap_open_live(device->name, 65536, 1, 0, errbuf)) == NULL) {
+				perror("pcap_open_live error");
+				exit(1);
+			} else {
+				printf("Connect to %s - done\n", device->name);
+			}
+		}
+		device = device->next;
+		++count;
+	}
+	pcap_freealldevs(alldevsp);
 	return 0;
+}
+
+void listDevice(pcap_if_t *alldevsp) {
+	pcap_if_t *device;
+	int i = 0;
+	printf("\nDEVICE LIST\n\n");
+	for (device = alldevsp; device != NULL; device = device->next) {
+		printf("%d) Device name: %s\n", i, device->name);
+		++i;
+	}
 }
